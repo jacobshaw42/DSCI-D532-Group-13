ccf60364883f9fb5518763366dbfda1f117cfa79
jacobshaw42 <jacob.shaw42@gmail.com>
2023-07-23 00:30:38 -0500

    modifing startup script to create static/files directory if necessary

--- start_up.sh
+++ start_up.sh
@@ -6,4 +6,10 @@ else
     python3 db_populate.py
 fi
 
+if [ ! -d "static/files" ]; then
+    echo "static/files directory did not exist, creating it now"
+    mkdir static/files
+else
+    echo "static/files exists"
+fi
 python3 app.py
